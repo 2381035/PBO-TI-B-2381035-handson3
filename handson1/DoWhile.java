4b516e4ed93f7521f537c82f9281317e39358a9d
Angga siahaan <2381035@unai.edu>
2024-09-15 21:55:43 +0700

    PBO-TI-B-2381035-handson3

---
+++ handson1/DoWhile.java
@@ -0,0 +1,10 @@
+package handson1;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        String[] KumpulanBuah = {"Apel", "Pisang", "Mangga", "Jeruk", "Durian", "Anggur"};
+        int counter = 0;
+            System.out.println("Elemen ke " + (counter + 1) + "Adalah : " + KumpulanBuah[counter]);
+            counter++;
+    }
+}
---
+++ handson1/ForEach.java
@@ -0,0 +1,10 @@
+package handson1;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] KumpulanBuah = {"Apel", "Pisang", "Mangga", "Jeruk", "Durian", "Anggur"};
+        for (String buah : KumpulanBuah){
+            System.out.println(buah);
+        }
+    }
+}
